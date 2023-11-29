f81232b89c22d91b2aa97e704fe1ad0c9a68e8d0
ashwini3735 <ashubeli3735@gmail.com>
2023-11-29 13:45:01 +0530

    ok

---
+++ src/test/java/com/abcd/test1.java
@@ -0,0 +1,11 @@
+package com.abcd;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void test1() {
+		System.out.println("--test1---");
+	}
+}
---
+++ src/test/java/com/abcd/test2.java
@@ -0,0 +1,12 @@
+package com.abcd;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void test2()
+	{
+		System.out.println("--test2---");
+	}
+
+}
